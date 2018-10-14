1c185a3211053acc73f17f719c31c02bc150a455
Carol Greene <carolgreene3@gmail.com>
2018-10-14 15:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 // Write your solution in this file!
-var customerName = 'bob';
-const leastFavoriteCustomer = 'bill';
-
-function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase()
-}
-
-function setBestCustomer() {
-  bestCustomer = 'not bob'
-}
-
-function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob'
-}
-
-function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = 'crabby guy'
-}
